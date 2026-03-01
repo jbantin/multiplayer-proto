3a8d3589abdd8917ba76c0c691d57de9e02176ad
jbantin <jbantin@gmx.de>
2026-03-01 21:30:13 +0100

    feat: enhance Enemy class with image rendering and animation frames

--- public/Enemy.ts
+++ public/Enemy.ts
@@ -1,5 +1,10 @@
-import type { Velocity } from "../types";
-import type { CameraInterface, EnemyInterface, EnemyConstructorParams } from "./types";
+import type { Velocity, } from "../types";
+import type { AnimationFrame } from "./types";
+import type {
+  CameraInterface,
+  EnemyInterface,
+  EnemyConstructorParams,
+} from "./types";
 
 export default class Enemy implements EnemyInterface {
   x: number;
@@ -9,24 +14,67 @@ export default class Enemy implements EnemyInterface {
   velocity: Velocity;
   ctx: CanvasRenderingContext2D;
   health?: number;
+  img: HTMLImageElement;
+  frames: AnimationFrame[];
+  frameCount: number;
+  delay: number;
+  delayCount: number;
 
-  constructor({ x, y, radius, color = "red", velocity, ctx }: EnemyConstructorParams) {
+  constructor({
+    x,
+    y,
+    radius,
+    color = "red",
+    velocity,
+    ctx,
+  }: EnemyConstructorParams) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
     this.ctx = ctx;
+    this.img = new Image();
+    this.img.src = "./assets/slime_purple.png";
+    this.frames = [
+      { x: 5, y: 33, xx: 5 },
+      { x: 30, y: 33, xx: 30 },
+      { x: 55, y: 33, xx: 55 },
+      { x: 80, y: 33, xx: 80 },
+    ];
+    this.frameCount = 0;
+    this.delay = 20;
+    this.delayCount = 0;
   }
 
   draw(camera: CameraInterface): void {
     this.ctx.beginPath();
-    this.ctx.arc(this.x - camera.x, this.y - camera.y, this.radius, 0, Math.PI * 2);
-    this.ctx.fillStyle = this.color;
-    this.ctx.fill();
+    this.ctx.arc(
+      this.x - camera.x,
+      this.y - camera.y,
+      this.radius,
+      0,
+      Math.PI * 2,
+    );
+    this.ctx.drawImage(
+      this.img,
+      this.frames[this.frameCount].x,
+      this.frames[this.frameCount].y,
+      14,
+      14,
+      this.x - camera.x - this.radius,
+      this.y - camera.y - this.radius,
+      this.radius * 2,
+      this.radius * 2,
+    );
   }
 
   update(camera: CameraInterface): void {
+    this.delayCount++;
+    if (this.delayCount >= this.delay) {
+      this.frameCount = (this.frameCount + 1) % this.frames.length;
+      this.delayCount = 0;
+    }
     this.draw(camera);
   }
 }
